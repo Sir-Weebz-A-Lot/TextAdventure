3c9c868acfd94ab570e650da26bbeb85283a1bff
Sir-Weebz-A-Lot <Sir-Weebz-A-Lot@users.noreply.github.com>
2017-08-14 13:14:23 -0700

    Changed some dialogue

--- Program.cs
+++ Program.cs
@@ -33,7 +33,7 @@ namespace TextAdventure
                 Console.WriteLine("(Press enter to continue)");
                 Console.ReadLine();
 
-                Console.WriteLine("Now that you've woke up, we will run you through some... tests");
+                Console.WriteLine("Now that you've woke up, we will run you through some tests");
                 Console.WriteLine("(Press enter to continue)");
                 Console.ReadLine();
 
@@ -41,26 +41,24 @@ namespace TextAdventure
                 Console.WriteLine("(Press enter to continue)");
                 Console.ReadLine();
 
-                Console.WriteLine("*The doctor shouts at you the following* 'WHY ARE WE STILL HERE?!  JUST TO SUFFER!'");
+                Console.WriteLine("You were in a horrible accident.  We'll need to test how you walk and such.");
                 Console.WriteLine("(Press enter to continue)");
                 Console.ReadLine();
 
-                Console.WriteLine("Every night...  I can feel my arm... and my leg... the comrades we've lost...");
+                Console.WriteLine("*the doctor helps you out of the bed and up to your feet; you feel nauseous and dizzy*");
                 Console.WriteLine("(Press enter to continue)");
                 Console.ReadLine();
 
-                Console.WriteLine("You feel it too, don't you, " + name);
+                Console.WriteLine("We've been keeping your muscle mass over the years via therapy, " + name);
                 Console.WriteLine("(Press enter to continue)");
                 Console.ReadLine();
 
-                Console.WriteLine("Y/N");
+                Console.WriteLine("You should be fine after all of this time.  *you stand up and use the bed as a crutch as you try to walk towards the door*");
                 Console.ReadLine();
 
-                if (answer == "Y")
-                    Console.WriteLine("...");
+                Console.WriteLine(".....");
+                Console.ReadLine();
 
-                else if (answer == "N")
-                    Console.WriteLine("...");
                }
 
             
