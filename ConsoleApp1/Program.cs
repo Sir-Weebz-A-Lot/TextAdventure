4e0d9a83b6a817f469199cec913ca2ae2febf8d3
Sir-Weebz-A-Lot <Sir-Weebz-A-Lot@users.noreply.github.com>
2017-08-15 01:11:12 -0700

    Significant changes to dialogue and added greeting

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,134 +10,172 @@ namespace TextAdventure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What's your Name?");
+            Console.WriteLine("Welcome to (unnamed) text adventure!  We hope you enjoy it");
+            Console.WriteLine("Now tell us your name");
             string name;
             name = Console.ReadLine();
             Console.WriteLine();
             Console.WriteLine("Hello, " + name);
             Console.WriteLine();
+            Console.WriteLine(">Adventure start<");
+            Console.WriteLine("(Press enter/space to continue)");
+            Console.ReadKey();
 
             {
                 bool wrongInput = true;
                 while (wrongInput)
                 {
-                    Console.WriteLine("Can you hear me?");
-                    string answer = Console.ReadLine();
-                    Console.WriteLine();
-                    if (answer == "Yes")
+                    Console.WriteLine(name + ", wake up! Can you hear me?");
+                    Console.WriteLine("1) Yes 2) No");
+                    if (Console.ReadKey().Key == ConsoleKey.D1)
                     {
-                        Console.WriteLine("Good, we can get started.");
+                        Console.WriteLine();
+                        Console.WriteLine("Get up, we need to go, now!");
                         Console.WriteLine();
                         wrongInput = false;
                     }
 
-                    else if (answer == "No")
+                    else if (Console.ReadKey().Key == ConsoleKey.D2)
                     {
-                        Console.WriteLine(">You slept another 7 years<");
-                        Console.WriteLine("(Press enter to quit)");
-                        Console.ReadLine();
+                        Console.WriteLine();
+                        Console.WriteLine(">Since you couldn't hear him, you died horribly");
+                        Console.WriteLine("(Press any key to quit...)");
+                        Console.ReadKey(); // check for button press before exiting
                         Environment.Exit(0);
+                        break; // just incase loop wants to repeat
                     }
 
                 }
             }
             {
 
-                Console.WriteLine("You've been in a coma for a year, " + name);
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine("We're under attack, they've found us");
+                Console.WriteLine("(Press enter to continue...)");
 
-                Console.WriteLine("Now that you've woke up, we will run you through some tests");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.ReadKey();
 
-                Console.WriteLine(">You're overwhelmed with pain<");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine(">As you wake up you notice the room around you - it's filled with weapons, the walls are horribly damaged and you hear what sounds like gunfire coming from outside");
+                Console.WriteLine("(Press enter to continue...)");
 
-                Console.WriteLine("You were in a horrible accident, we'll need to test how you walk and such");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.ReadKey();
 
-                Console.WriteLine(">the doctor helps you out of the bed and up to your feet; you feel nauseous and dizzy<");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine(">You're overwhelmed with pain");
+                Console.WriteLine("(Press enter to continue...)");
+
+                Console.ReadKey();
+
+                Console.WriteLine("Come on, through here");
+                Console.WriteLine("(Press enter to continue...)");
+                Console.ReadKey();
+
+                Console.WriteLine(">You see a firearm sitting next to you");
+                Console.WriteLine("(Press enter to continue...)");
+                Console.ReadKey();
+
+                Console.WriteLine("Do you pick it up or leave it there?");
+                Console.WriteLine("(take/leave)");
+                string answer;
+                answer = Console.ReadLine();
+
+                if (answer == "take")
+                {
+                    Console.WriteLine(">You take the firearm and rush to the door");
+                    Console.WriteLine("(Press enter to continue)");
+                }
+                else if (answer == "leave")
+                {
+                    Console.WriteLine(">You leave the firearm and rush to the door, hoping you don't need it");
+                }
+                
+
 
                 Console.WriteLine("We've been keeping your muscle mass over the years via therapy, " + name);
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine("(Press any key to continue...)");
+                Console.ReadKey();
 
-                Console.WriteLine("You should be fine after all of this time.  >you stand up and use the bed as a crutch as you try to walk towards the door<");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine("You should be fine after all of this time.");
+                Console.WriteLine(">you stand up and use the bed as a crutch as you try to walk towards the door");
+                Console.WriteLine("(press any key to continue...)");
+                Console.ReadKey();
 
-                Console.WriteLine(">You suddenly pass out, hitting the floor with your head<");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine(">You suddenly pass out, hitting the floor with your head");
+                Console.WriteLine("(press any key to continue...)");
+                Console.ReadKey();
 
-                Console.WriteLine(">You wake up in a the hospital bed once again, this time much sooner than before, a nurse is checking up on you and you try to get her attention<");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine(">You wake up in a the hospital bed once again, this time much sooner than before, a nurse is checking up on you and you try to get her attention");
+                Console.WriteLine("(press any key to continue...)");
+                Console.ReadKey();
 
-                Console.WriteLine(">The nurse went off to get the doctor, five minutes later he arrives<");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine(">The nurse went off to get the doctor, five minutes later he arrives");
+                Console.WriteLine("(press any key to continue...)");
+                Console.ReadKey();
 
 
-                Console.WriteLine(">The doctor walks in and looks at you, you sit up in the bed and watch what he's doing<");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine(">The doctor walks in and looks at you, you sit up in the bed and watch what he's doing");
+                Console.WriteLine("(Press any key to continue...)");
+                Console.ReadKey();
 
                 Console.WriteLine("You have sustained major head trauma from the accident I told you about...");
                 Console.WriteLine("You barely survived. We had to operate on you for a full week.");
-                Console.WriteLine(">You then hear people banging on the door<");
-                Console.WriteLine("Oh no! There are people here for your head! >you suddenly feel an overwhelming sense of tiredness; you nod off<");
-                Console.WriteLine("(Press enter to continue)");
-                Console.ReadLine();
+                Console.WriteLine(">You then hear people banging on the door");
+                Console.WriteLine("Oh no! There are people here for your head!");
+                Console.WriteLine(">you suddenly feel an overwhelming sense of tiredness; you nod off");
+                Console.WriteLine("(Press any key to continue...)");
+                Console.ReadKey();
 
                 Console.WriteLine(name + ",can you hear me?  Wake up!");
                 Console.WriteLine(">You wake up, not knowing where you are or even who the person who woke you up is");
-                Console.WriteLine("(You have four choices to ask the person: 1.) 'Where are we?' 2.) 'Who are you?' 3.) 'What happened?' 4.) 'I have no questions to ask.'");
 
                 {
-                    bool wrongInput = true;
-                    while (wrongInput)
+                 
                     {
-                        Console.WriteLine();
-                        string answer = Console.ReadLine();
-                        if (answer == "1")
+                        Console.WriteLine("(You have four choices to ask the person:");
+                        Console.WriteLine("1.) 'Where are we?' 2.) 'Who are you?' 3.) 'What happened?' 4.) 'I have no questions to ask.'");
+                        if (Console.ReadKey().Key == ConsoleKey.D1)
                         {
                             Console.WriteLine();
                             Console.WriteLine("We're... trapped inside of a slaughterhouse..");
+                            Console.WriteLine();
+                            
                         }
 
-                        else if (answer == "2")
+                        else if (Console.ReadKey().Key == ConsoleKey.D2)
                         {
                             Console.WriteLine();
                             Console.WriteLine("I don't think that's important right now");
+                            Console.WriteLine();
+                            
                         }
 
-                        else if (answer == "3")
+                        else if (Console.ReadKey().Key == ConsoleKey.D3)
                         {
                             Console.WriteLine();
                             Console.WriteLine("I'm not sure...");
+                            Console.WriteLine();
+                            
                         }
 
-                        else if (answer == "4")
+                        else if (Console.ReadKey().Key == ConsoleKey.D4)
                         {
                             Console.WriteLine();
                             Console.WriteLine("...");
-                            break;
+                            Console.WriteLine();
+                           
                         }
 
                         else
                             Console.WriteLine("...");
+                        
                     }
-
+                    Console.WriteLine("Press any button to exit...");
+                    Console.ReadKey(); // checks for button press
                 }
+
+                Console.WriteLine("We were trapped in here by some... psycho");
+
                 Console.ReadLine();
-                Console.ReadLine();
-                Console.ReadLine();
+                Console.ReadLine(); // For room after text for (input)
+                Console.ReadLine(); 
 
 
             }
